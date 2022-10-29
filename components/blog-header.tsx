bee13bcb7b3fee7970ab17cab8ded627b4071c5e
cneis <96124560+cneis@users.noreply.github.com>
2022-10-29 22:23:10 +0200

    logo wieder raus

--- components/blog-header.tsx
+++ components/blog-header.tsx
@@ -4,10 +4,10 @@ import Logo from "../images/logo_samtpfoten.png"
 export default function BlogHeader({ title }) {
   return (
     <section className="mt-16 mb-10 flex flex-col items-center md:mb-16 md:flex-row md:justify-between">
-      <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
+      <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-7xl">
         {title}
       </h1>
-     <img src={"../images/logo_samtpfoten.png"} width="100px" height="100px" />
+     
     </section>
   )
 }
